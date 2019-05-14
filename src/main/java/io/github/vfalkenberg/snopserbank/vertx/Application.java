8729007611b0b585bee0985d6678e2547b7e5541
Viktor.Falkenberg <Viktor.Falkenberg@mediascope.net>
2019-05-14 10:11:14 +0300

    Initial commit

---
+++ src/main/java/io/github/vfalkenberg/snopserbank/vertx/Application.java
@@ -0,0 +1,12 @@
+package io.github.vfalkenberg.snopserbank.vertx;
+
+import io.vertx.core.Vertx;
+
+/**
+ * @author Виктор Фалькенберг
+ */
+public class Application {
+    public static void main(String[] args){
+        final Vertx vertx = Vertx.vertx();
+    }
+}
